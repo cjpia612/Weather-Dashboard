15b9472b47a2a30fa43b71c5df064f997978614f
Cattleya Pia <cjpia612@gmail.com>
2020-05-20 23:52:52 -0700

    added document ready functiona and started on click function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
+$(document).ready(function(){
+
+    $(".btn").click(function(){
+
+    });
+});
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
