faf5f2966d293ee8c4d22ed43e4e97471d79fdfb
Cattleya Pia <cjpia612@gmail.com>
2020-05-23 20:36:51 -0700

    added prepend function, functional; getter properly stores

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
     var lat = [];
     var lon = [];
+    var cityInput = [];
 
     $(".btn").click(function(){
+        var city= $("#cityInput").val();
         event.preventDefault();
         currentWeather();
         fiveDay();
+        prepend(city);
+        console.log(city);
     });
 
     function currentWeather(){
         var city= $("#cityInput").val();
+        cityInput.push(city);
+        localStorage.setItem("pastCities", JSON.stringify(cityInput));
         var currentDate = moment().format("MMMM Do YYYY");
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=6b8ef924d7aa3ffc3be582be83541797",
@@ -102,15 +108,24 @@ $(document).ready(function(){
         });
         
     }
+
+    function prepend(blahInput){
+        var listCity= $("<li>");
+        listCity.addClass("list-group-item");
+        listCity.text(blahInput);
+        $("#cityItems").prepend(listCity);
+    }
+
+    function getter(){
+        var inputSearches = JSON.parse(localStorage.getItem(cityInput));
+        if (inputSearches == null){
+            inputSearches = [];
+        } else {
+            for (let i= 0; i < inputSearches.length; i++){
+                prepend(inputSearches);
+            }
+        }
+    }
+    getter();
 });
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
+
